8d7b8b7021a02f3d988a8afa12d1c4778e7fdd03
glottologist <glottologist@users.noreply.github.com>
2026-08-07 08:32:44 +0000

    deploy: 7834e82588860aaf780cec1366524456a70898d7

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-36b52500.js',
+                path_to_root + 'searchindex-ca0324a7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
